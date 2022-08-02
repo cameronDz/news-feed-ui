13bf1d04e852044fc1efca31fda384fe2453cc01
Cam Dziurgot <camerondziurgot@gmail.com>
2022-08-01 22:48:44 -0400

    add more gaming feeds, update packages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.5.0] 2022-08-01
+
+### Added
+
+- additional gaming feeds
+
 ## [0.5.0] 2022-06-22
 
 ### Added
--- assets/feeds.js
+++ assets/feeds.js
@@ -16,6 +16,14 @@ const feeds = {
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCR9X2tT0_WVfuYO17vqstlA",
     // Video Donkey
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCsvn_Po0SmunchJYOWpOxMg",
+    // The Salt Factory
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCQrDV_RiKJ-cNyyMdTjzREQ",
+    // KingK
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UC18YhnNvyrU2kTwCyj9p5ag",
+    // Joseph Anderson
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCyhnYIvIKK_--PiJXCMKxQQ",
+    // Luke Stephens
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCe4_UPAmhz2sqorGbF8oZHA",
   ],
   "Techno Music": [
     // Club Music
@@ -26,5 +34,88 @@ const feeds = {
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCAHlZTSgcwNNpf8LV3E6kDQ",
   ],
 };
+
+const feedCollections = [
+  {
+    name: "Comedy",
+    feeds: [
+      {
+        name: "Tim Dillon Show",
+        distributor: "YOUTUBE",
+        channelId: "UC4woSp8ITBoYDmjkukhEhxg",
+      },
+      {
+        name: "Tuesdays with Stories",
+        distributor: "YOUTUBE",
+        channelId: "UCsE74YJvPJpaquzTPMO8hAA",
+      },
+      {
+        name: "Red Letter Media",
+        distributor: "YOUTUBE",
+        channelId: "UCrTNhL_yO3tPTdQ5XgmmWjA",
+      },
+    ],
+  },
+  {
+    name: "Techno Music",
+    feeds: [
+      {
+        name: "Club Music",
+        distributor: "YOUTUBE",
+        channelId: "UComEqi_pJLNcJzgxk4pPz_A",
+      },
+      {
+        name: "DJ Ekki",
+        distributor: "YOUTUBE",
+        channelId: "UCPWBlX15fNBUw0cLqKM-V7g",
+      },
+      {
+        name: "Electric Dance Mixes",
+        distributor: "YOUTUBE",
+        channelId: "UCAHlZTSgcwNNpf8LV3E6kDQ",
+      },
+    ],
+  },
+  {
+    name: "Gaming",
+    feeds: [
+      {
+        name: "Game Theorist",
+        distributor: "YOUTUBE",
+        channelId: "UCo_IB5145EVNcf8hw1Kku7w",
+      },
+      {
+        name: "Josh Strife",
+        distributor: "YOUTUBE",
+        channelId: "UCR9X2tT0_WVfuYO17vqstlA",
+      },
+      {
+        name: "Video Donkey",
+        distributor: "YOUTUBE",
+        channelId: "UCsvn_Po0SmunchJYOWpOxMg",
+      },
+      {
+        name: "The Salt Factory",
+        distributor: "YOUTUBE",
+        channelId: "UCQrDV_RiKJ-cNyyMdTjzREQ",
+      },
+      {
+        name: "KingK",
+        distributor: "YOUTUBE",
+        channelId: "UC18YhnNvyrU2kTwCyj9p5ag",
+      },
+      {
+        name: "Joseph Anderson",
+        distributor: "YOUTUBE",
+        channelId: "UCyhnYIvIKK_--PiJXCMKxQQ",
+      },
+      {
+        name: "Luke Stephens",
+        distributor: "YOUTUBE",
+        channelId: "UCe4_UPAmhz2sqorGbF8oZHA",
+      },
+    ],
+  },
+];
 
 export default feeds;
